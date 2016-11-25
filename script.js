aba95c97b8583914f9bff58b678051bc9d0a1196
GermainZ <germanosz@gmail.com>
2016-11-25 23:01:49 +0200

    editable location

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 window.onload = function() {
     ajax_get("data.json", display_data);
-    var weather_query = "select item.condition from weather.forecast where woeid=1962088 and u='c'"
-    var weather_url = "https://query.yahooapis.com/v1/public/yql?q=" + escape(weather_query) + "&format=json&env=store"
-    ajax_get(weather_url, display_weather);
+    document.getElementById("weather_location").onkeypress = function(event) {
+        if (event.which == 13) {
+            get_weather();
+        }
+    };
+    get_weather();
 }
 
 function ajax_get(url, callback) {
@@ -10,6 +13,7 @@ function ajax_get(url, callback) {
     xmlhttp.onreadystatechange = function() {
         if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
             try {
+                console.log(xmlhttp.responseText);
                 var data = JSON.parse(xmlhttp.responseText);
             } catch (err) {
                 console.log(err.message);
@@ -35,7 +39,27 @@ function display_data(data) {
 }
 
 function display_weather(data) {
+    if (data.query.count === 0) {
+        alert("Couldn't find city!");
+        document.getElementById("weather_condition").innerHTML = "☺";
+        document.getElementById("weather_temperature").innerHTML = "☺";
+        return;
+    }
     var condition = data.query.results.channel.item.condition;
     document.getElementById("weather_condition").innerHTML = condition.text;
     document.getElementById("weather_temperature").innerHTML = condition.temp;
+}
+
+function get_weather() {
+    var text = document.getElementById("weather_location").value;
+    if (text) {
+        var weather_query = "select item.condition from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + text + "') and u='c'"
+        var weather_url = "https://query.yahooapis.com/v1/public/yql?q=" + escape(weather_query) + "&format=json&env=store"
+        document.getElementById("weather_condition").innerHTML = "…";
+        document.getElementById("weather_temperature").innerHTML = "…";
+        ajax_get(weather_url, display_weather);
+    } else {
+        document.getElementById("weather_condition").innerHTML = "☺";
+        document.getElementById("weather_temperature").innerHTML = "☺";
+    }
 }
